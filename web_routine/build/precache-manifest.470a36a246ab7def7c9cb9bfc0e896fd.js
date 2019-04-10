60841fc3895559550789fe7d257ce09f7ac663cd
Liu Yanbo <yanbo.liu@ucarinc.com>
2019-04-10 09:49:00 +0800

    optimize

---
+++ web_routine/build/precache-manifest.470a36a246ab7def7c9cb9bfc0e896fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b150ce64a4e537f4ce53",
+    "url": "/static/js/main.7089dc6a.chunk.js"
+  },
+  {
+    "revision": "67e89a0c18d302f74b98",
+    "url": "/static/js/2.57bb8849.chunk.js"
+  },
+  {
+    "revision": "b150ce64a4e537f4ce53",
+    "url": "/static/css/main.884a84ab.chunk.css"
+  },
+  {
+    "revision": "67e89a0c18d302f74b98",
+    "url": "/static/css/2.24fbba08.chunk.css"
+  },
+  {
+    "revision": "9f6c08081d9dee7de0974fe082aab8b5",
+    "url": "/index.html"
+  }
+];
